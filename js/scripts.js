5d8746496b17e72005c617ccf31caae218983044
Pip Bbaale <namugera.philip@gmail.com>
2020-02-18 09:31:40 +0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+jQuery(document).ready(function() {
 jQuery('h1').click(function(){
-    alert("This is a heading tag")
+    alert("This is a heading tag");
+    alert("I told you, THIS IS A HEADER!");
   });
 
   jQuery('p').click(function(){
-    alert("This is a paragraph")
+    alert("This is a paragraph");
   });
 
   jQuery('img').click(function(){
-    alert("This is an image")
+    alert("This is an image");
   });
+});
